ba188b6f65858cbbd7685065440161fa30d6c355
Marc Nuri <marc@marcnuri.com>
2018-03-23 07:29:46 +0100

    Added test cases

--- src/main/java/com/marcnuri/demo/listtomap/GithubRepo.java
+++ src/main/java/com/marcnuri/demo/listtomap/GithubRepo.java
@@ -5,6 +5,9 @@
  */
 package com.marcnuri.demo.listtomap;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * https://api.github.com/orgs/marcnuri-demo/repos
  * Created by Marc Nuri <marc@marcnuri.com> on 2018-03-22.
@@ -15,8 +18,12 @@ public class GithubRepo {
 //  Fields
 //**************************************************************************************************
 	private String name;
+	@JsonProperty("full_name")
 	private String fullName;
 	private String description;
+	private Boolean fork;
+	@JsonIgnore
+	private Integer localVersion;
 
 //**************************************************************************************************
 //  Constructors
@@ -29,6 +36,30 @@ public class GithubRepo {
 //**************************************************************************************************
 //  Overridden Methods
 //**************************************************************************************************
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		GithubRepo that = (GithubRepo) o;
+
+		if (name != null ? !name.equals(that.name) : that.name != null) return false;
+		if (fullName != null ? !fullName.equals(that.fullName) : that.fullName != null) return false;
+		if (description != null ? !description.equals(that.description) : that.description != null) return false;
+		if (fork != null ? !fork.equals(that.fork) : that.fork != null) return false;
+		return localVersion != null ? localVersion.equals(that.localVersion) : that.localVersion == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+		result = 31 * result + (description != null ? description.hashCode() : 0);
+		result = 31 * result + (fork != null ? fork.hashCode() : 0);
+		result = 31 * result + (localVersion != null ? localVersion.hashCode() : 0);
+		return result;
+	}
+
 
 //**************************************************************************************************
 //  Other Methods
@@ -37,8 +68,46 @@ public class GithubRepo {
 //**************************************************************************************************
 //  Getter/Setter Methods
 //**************************************************************************************************
+	public String getName() {
+		return name;
+	}
 
-//**************************************************************************************************
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getFork() {
+		return fork;
+	}
+
+	public void setFork(Boolean fork) {
+		this.fork = fork;
+	}
+
+	public Integer getLocalVersion() {
+		return localVersion;
+	}
+
+	public void setLocalVersion(Integer localVersion) {
+		this.localVersion = localVersion;
+	}
+	//**************************************************************************************************
 //  Static Methods
 //**************************************************************************************************
 
---
+++ src/test/java/com/marcnuri/demo/listtomap/ListToMapTest.java
@@ -0,0 +1,137 @@
+/*
+ * ListToMapTest.java
+ *
+ * Created on 2018-03-23, 6:57
+ */
+package com.marcnuri.demo.listtomap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Marc Nuri <marc@marcnuri.com> on 2018-03-23.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Import({ListToMapTest.ListToMapConfiguration.class})
+public class ListToMapTest {
+
+//**************************************************************************************************
+//  Fields
+//**************************************************************************************************
+	private static final String CURRENT_REPO_NAME = "java8-streams-list-to-map";
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+//**************************************************************************************************
+//  Constructors
+//**************************************************************************************************
+
+//**************************************************************************************************
+//  Abstract Methods
+//**************************************************************************************************
+
+//**************************************************************************************************
+//  Overridden Methods
+//**************************************************************************************************
+
+//**************************************************************************************************
+//  Other Methods
+//**************************************************************************************************
+	@Test
+	public void listToMap_noDuplicatesList_shouldReturnOk() {
+		// Given
+		final String marcnuriDemoReposUrl = "https://api.github.com/orgs/marcnuri-demo/repos";
+		final List<GithubRepo> repos = restTemplate.exchange( marcnuriDemoReposUrl, HttpMethod.GET,
+				new HttpEntity<>(new HttpHeaders()), new ParameterizedTypeReference<List<GithubRepo>>(){}).getBody();
+
+		// When
+		final Map<String, GithubRepo> repoMap = repos.stream().collect(Collectors.toMap(GithubRepo::getName, Function.identity()));
+
+		// Then
+		assertFalse(repoMap.isEmpty());
+		assertNotNull(repoMap.get(CURRENT_REPO_NAME));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void listToMap_duplicatesList_shouldThrowException() {
+		// Given
+		final String marcnuriDemoReposUrl = "https://api.github.com/orgs/marcnuri-demo/repos";
+		final List<GithubRepo> repos = new ArrayList<>();
+		for(int it = 0; it < 2; it++) {
+			repos.addAll(restTemplate.exchange( marcnuriDemoReposUrl, HttpMethod.GET,
+					new HttpEntity<>(new HttpHeaders()), new ParameterizedTypeReference<List<GithubRepo>>(){}).getBody());
+		}
+
+		// When
+		final Map<String, GithubRepo> repoMap = repos.stream().collect(Collectors.toMap(GithubRepo::getName, Function.identity()));
+
+		// Then
+		fail();
+	}
+
+	@Test()
+	public void listToMapHandleDuplicates_duplicatesList_shouldReturnOk() {
+		// Given
+		final String marcnuriDemoReposUrl = "https://api.github.com/orgs/marcnuri-demo/repos";
+		final List<GithubRepo> repos = new ArrayList<>();
+		for(int it = 0; it < 2; it++) {
+			final List<GithubRepo> temp = restTemplate.exchange( marcnuriDemoReposUrl, HttpMethod.GET,
+					new HttpEntity<>(new HttpHeaders()), new ParameterizedTypeReference<List<GithubRepo>>(){}).getBody();
+			for(GithubRepo ghr : temp) {
+				 ghr.setLocalVersion(it);
+			}
+			repos.addAll(temp);
+		}
+
+		// When
+		final Map<String, GithubRepo> repoMap = repos.stream().collect(Collectors.toMap(GithubRepo::getName, Function.identity(),
+				(ghrPrevious, ghrNew) -> ghrNew));
+
+		// Then
+		final int newestVersion = 1;
+		assertFalse(repoMap.isEmpty());
+		final GithubRepo currentRepo = repoMap.get(CURRENT_REPO_NAME);
+		assertNotNull(currentRepo);
+		assertEquals(newestVersion, currentRepo.getLocalVersion().intValue());
+	}
+//**************************************************************************************************
+//  Getter/Setter Methods
+//**************************************************************************************************
+
+//**************************************************************************************************
+//  Static Methods
+//**************************************************************************************************
+
+//**************************************************************************************************
+//  Inner Classes
+//**************************************************************************************************
+	@Configuration
+	protected static class ListToMapConfiguration {
+
+		@Bean
+		public RestTemplate restTemplate() {
+			final RestTemplate restTemplate = new RestTemplate();
+			return restTemplate;
+		}
+
+	}
+}
